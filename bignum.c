541ab3bedb32c8549cb7b6bf5ce05923ce8c9d1a
ilaybargil-coder <ilaybargil@gmail.com>
2025-11-20 22:16:55 +0200

    Refactor bignum.c for consistency and clarity
    עבודה של 100

--- bignum.c
+++ bignum.c
@@ -13,7 +13,7 @@ static Link* node_new(int d) {
 }
 
 BigNum* bn_from_string(const char* s) {
-    if (!s) 
+    if (!s)
         return NULL;
     BigNum* num = malloc(sizeof(BigNum));
     num->head = NULL;
@@ -25,7 +25,7 @@ BigNum* bn_from_string(const char* s) {
     if(*s == '\0')
     {
         num->head = num->tail = node_new(0);
-        return num;  
+        return num;
     }
     while (*s != '\0')
     {
@@ -33,7 +33,7 @@ BigNum* bn_from_string(const char* s) {
             Link* node = node_new(digit);
             if (num->head == NULL) {
                 num->head = num->tail = node;
-            } 
+            }
             else {
                 num->tail->next = node;
                 node->prev = num->tail;
@@ -42,17 +42,17 @@ BigNum* bn_from_string(const char* s) {
             s++;
 
     }
-        
-    return num;   
-}        
+
+    return num;
+}
 char* bn_to_string(const BigNum* num){
-        if (num->head == NULL) 
+        if (num->head == NULL)
         return NULL;
         int len=0;
         Link* p = num->head;
         while (p != NULL) {
-            len++; 
-            p = p->next;         
+            len++;
+            p = p->next;
         }
         char* s = malloc((len + 1) * sizeof(char));
         p = num->head;
@@ -60,7 +60,7 @@ char* bn_to_string(const BigNum* num){
         while (p != NULL) {
             s[i] = '0' + p->digit;
             i++;
-            p = p->next;         
+            p = p->next;
         }
         s[len] = '\0';
         return s;
@@ -71,12 +71,12 @@ int bn_compare(const BigNum* A, const BigNum* B) {
         Link* pB = B->head;
         int lenB=0,lenA = 0;
         while (pA != NULL) {
-            lenA++; 
-            pA = pA->next;         
+            lenA++;
+            pA = pA->next;
         }
         while (pB != NULL) {
-            lenB++; 
-            pB = pB->next;         
+            lenB++;
+            pB = pB->next;
         }
         pA = A->head;
         pB = B->head;
@@ -92,7 +92,7 @@ int bn_compare(const BigNum* A, const BigNum* B) {
                 if(pB->digit > pA->digit)
                    return -1;
                 pA = pA->next;
-                pB = pB->next;  
+                pB = pB->next;
             }
         }
         return 0;
@@ -100,20 +100,173 @@ int bn_compare(const BigNum* A, const BigNum* B) {
 
 /* addition */
 BigNum* bn_add(const BigNum* A, const BigNum* B) {
+    BigNum* res = malloc (sizeof(BigNum));
+    if (!res) return NULL;// checking for memory not null
+    res->head = res->tail = NULL;//res equal null in the start
+    Link* a = A->tail;
+    Link* b = B->tail;
+    int carry = 0;
+    while (a != NULL || b != NULL||carry!=0) {
+        int da,db;//digit a digit b
+        if (a != NULL)
+            da = a->digit;
+        else
+            da = 0;
 
+        if (b != NULL)
+            db = b->digit;
+        else
+            db = 0;
+
+        int sum = da + db + carry;// calc sum of digits+carry
+        int digit = sum % 10;
+        carry = sum / 10;
+        Link* node = node_new(digit);
+
+        if (res->head == NULL) {//first digit of our result
+            res->head = res->tail = node;
+        } else {//next digits untill null
+            node->next = res->head;
+            res->head->prev = node;
+            res->head = node;
+        }
+        //next digit to sum in a and b
+        if (a != NULL) a = a->prev;
+        if (b != NULL) b = b->prev;
+    }
+
+    return res;
 }
 
-/* subtraction: returns 0 if A < B */
+
+/* subtraction */
 BigNum* bn_sub(const BigNum* A, const BigNum* B) {
+    if (bn_compare(A, B) < 0) {
+        return bn_from_string("0");
+    }
+    BigNum* res = malloc(sizeof(BigNum));
+    if (!res)
+        return NULL;
+    res->head = res->tail = NULL;//res equal null in the start
+    Link* a = A->tail;
+    Link* b = B->tail;
+    int borrow = 0;
+    while (a != NULL || b != NULL) {
+        int da = 0;
+        int db = 0;
+        if (a != NULL)
+            da = a->digit;
+        else
+            da = 0;
+        if (b != NULL)
+            db = b->digit;
+        else
+            db = 0;
+        da = da - borrow;
+
+        if (da < db) {
+            da = da + 10;
+            borrow = 1;
+        } else {
+            borrow = 0;
+        }
+        int digit = da - db;
+        Link* node = node_new(digit);
+
+        if (res->head == NULL) {
+            res->head = res->tail = node;
+        } else {
+            node->next = res->head;
+            res->head->prev = node;
+            res->head = node;
+        }
+        if (a != NULL) a = a->prev;
+        if (b != NULL) b = b->prev;
+    }
+    //deletes zeros for example 000123 = 123
+    Link* p = res->head;
+    while (p != NULL && p->digit == 0 && p != res->tail) {
+        Link* to_free = p;
+        p = p->next;
+        p->prev = NULL;
+        res->head = p;
+        free(to_free);
+    }
+    return res;
 
 }
-
 /* multiply one digit with shift */
-static BigNum* bn_mul_single(const BigNum* A, int d, int shift) {
+BigNum* bn_mul_single(const BigNum* A, int d, int shift) {
+    BigNum* res = malloc(sizeof(BigNum));
+    res->head = res->tail = NULL;
 
+    Link* a = A->tail;
+    int carry = 0;
+
+    while (a != NULL || carry != 0) {
+        int da;
+        if (a != NULL)
+            da = a->digit;
+        else
+            da = 0;
+
+        int prod = da * d + carry;
+        int digit = prod % 10;
+        carry = prod / 10;
+
+        Link* node = node_new(digit);
+
+        if (res->head == NULL) {
+            res->head = res->tail = node;
+        } else {
+            node->next = res->head;
+            res->head->prev = node;
+            res->head = node;
+        }
+
+        if (a != NULL) a = a->prev;
+    }
+
+    for (int i = 0; i < shift; i++) {
+        Link* zero = node_new(0);
+        res->tail->next = zero;
+        zero->prev = res->tail;
+        res->tail = zero;
+    }
+
+    return res;
 }
 
 /* multiplication */
 BigNum* bn_mul(const BigNum* A, const BigNum* B) {
+    BigNum* result = bn_from_string("0");
+    int shift = 0;
 
+    for (Link* b = B->tail; b != NULL; b = b->prev) {
+        BigNum* partial = bn_mul_single(A, b->digit, shift);
+
+        BigNum* new_result = bn_add(result, partial);
+
+        bn_free(result);
+        bn_free(partial);
+
+        result = new_result;
+        shift++;
+    }
+
+    return result;
+}
+
+
+void bn_free(BigNum* a) {
+    if (!a) return;
+
+    Link* p = a->head;
+    while (p != NULL) {
+        Link* next = p->next;
+        free(p);
+        p = next;
+    }
+
+    free(a);
 }
